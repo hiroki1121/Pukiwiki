1817d35859a351f93a4bfd4971f019c126a40b4d
hiroki1121 <81612317+hiroki1121@users.noreply.github.com>
2021-06-15 21:12:45 +0900

    Update problem9.rb

--- problem9.rb
+++ problem9.rb
@@ -4,16 +4,19 @@
 # a + b + c = 1000 となるピタゴラスの三つ組が一つだけ存在する.
 # これらの積 abc を計算しなさい.
 
-array_a = [*1..998]
-array_b = [*2..999]
-array_c = [*3..1000]
-
-array_a.each do |a|
-  array_b.each do |b|
-    array_c.each do |c|
-      if (a ** 2 + b ** 2 == c ** 2) && (a + b + c == 1000) && a < b && b < c
-        puts a * b * c
+def verification
+  array_a = [*1..998]
+  array_b = [*2..999]
+  array_c = [*3..1_000]
+  array_a.each do |a|
+    array_b.each do |b|
+      array_c.each do |c|
+        if (a ** 2 + b ** 2 == c ** 2) && (a + b + c == 1_000) && a < b && b < c
+          return a * b * c
+        end
       end
     end
   end
 end
+
+puts verification
